552d6650b95f1688a0e3ede23a334618ab1c1aa0
manylovv <yellowmanylov@gmail.com>
2023-05-19 23:20:56 +0600

    add react-router library

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './pages/Home.tsx';
+import Home from './pages/home.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '*', element: <div>404</div> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
